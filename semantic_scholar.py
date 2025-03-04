ac1edcf0f26d00c4d979fff356a0e46bba0de39d
Nick Haupka <nick.haupka@outlook.de>
2025-03-04 09:46:52 +0100

    replace print statements with logging

--- semantic_scholar.py
+++ semantic_scholar.py
@@ -3,6 +3,7 @@ import functools
 import os
 from pathlib import Path
 import shutil
+import logging
 
 
 class SemanticScholar:
@@ -31,15 +32,15 @@ class SemanticScholar:
         papers = requests.get(url=f'http://api.semanticscholar.org/datasets/v1/release/{self.snapshot_date}/dataset/papers',
                               headers={'x-api-key': self.api_key}).json()
 
-        print('Download: papers')
+        logging.info('Download: papers')
 
         for n, file in enumerate(papers['files'], start=1):
-            print(f'Download file {n} of {len(papers["files"])}.')
+            logging.info(f'Download file {n} of {len(papers["files"])}.')
             with requests.get(file, stream=True) as response:
                 with open(f'{papers_download_path}/papers-part{n}.jsonl.gz', 'wb') as file:
                     response.raw.read = functools.partial(response.raw.read, decode_content=False)
                     shutil.copyfileobj(response.raw, file)
-            print(f'Successfully download file {n} of {len(papers["files"])}.')
+            logging.info(f'Successfully download file {n} of {len(papers["files"])}.')
 
     def download_venues(self):
 
@@ -47,18 +48,18 @@ class SemanticScholar:
 
         os.makedirs(venues_download_path, exist_ok=False)
 
-        print('Download: venues')
+        logging.info('Download: venues')
 
         venues = requests.get(url=f'http://api.semanticscholar.org/datasets/v1/release/{self.snapshot_date}/dataset/publication-venues',
                               headers={'x-api-key': self.api_key}).json()
 
         for n, file in enumerate(venues['files'], start=1):
-            print(f'Download file {n} of {len(venues["files"])}.')
+            logging.info(f'Download file {n} of {len(venues["files"])}.')
             with requests.get(file, stream=True) as response:
                 with open(f'{venues_download_path}/venues-part{n}.jsonl.gz', 'wb') as file:
                     response.raw.read = functools.partial(response.raw.read, decode_content=False)
                     shutil.copyfileobj(response.raw, file)
-            print(f'Successfully download file {n} of {len(venues["files"])}.')
+            logging.info(f'Successfully download file {n} of {len(venues["files"])}.')
 
     def download_abstracts(self):
 
@@ -66,18 +67,18 @@ class SemanticScholar:
 
         os.makedirs(abstracts_download_path, exist_ok=False)
 
-        print('Download: abstracts')
+        logging.info('Download: abstracts')
 
         abstracts = requests.get(url=f'http://api.semanticscholar.org/datasets/v1/release/{self.snapshot_date}/dataset/abstracts',
                               headers={'x-api-key': self.api_key}).json()
 
         for n, file in enumerate(abstracts['files'], start=1):
-            print(f'Download file {n} of {len(abstracts["files"])}.')
+            logging.info(f'Download file {n} of {len(abstracts["files"])}.')
             with requests.get(file, stream=True) as response:
                 with open(f'{abstracts_download_path}/abstracts-part{n}.jsonl.gz', 'wb') as file:
                     response.raw.read = functools.partial(response.raw.read, decode_content=False)
                     shutil.copyfileobj(response.raw, file)
-            print(f'Successfully download file {n} of {len(abstracts["files"])}.')
+            logging.info(f'Successfully download file {n} of {len(abstracts["files"])}.')
 
 
 if __name__ == '__main__':
